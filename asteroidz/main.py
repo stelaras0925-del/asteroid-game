d3ac7ae4c37573bc1550076c332384a9953f4ade
stelaras0925-del <stelaras0925@gmail.com>
2025-11-19 14:32:08 +0200

    Windows GUI added

--- asteroidz/main.py
+++ asteroidz/main.py
@@ -1,5 +1,21 @@
+import pygame
+from constants import SCREEN_WIDTH,SCREEN_HEIGHT
+from logger import log_state
+
 def main():
-    print("Hello from asteroidz!")
+    VERSION = pygame.version.ver
+    print(f"Starting Asteroids with pygame version: {VERSION}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen= pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
