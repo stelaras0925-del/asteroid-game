6e516e807857eb3c6cc0bddeebb27b82d893afb3
stelaras0925-del <stelaras0925@gmail.com>
2025-11-19 16:12:33 +0200

    Added triangle and rotation

--- asteroidz/main.py
+++ asteroidz/main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_WIDTH,SCREEN_HEIGHT
+from constants import SCREEN_WIDTH,SCREEN_HEIGHT,PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     VERSION = pygame.version.ver
@@ -9,14 +10,25 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen= pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = int(SCREEN_WIDTH / 2)
+    y = int(SCREEN_HEIGHT / 2)
+    player = Player(x,y,PLAYER_RADIUS)    
+    print(f"Player Radius: {player.radius}")
+    print(f"Player XY: {player.position}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         log_state()
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-
-
+        clock.tick(60)
+        dt = clock.tick(60)/1000
+        
+        
 if __name__ == "__main__":
     main()
